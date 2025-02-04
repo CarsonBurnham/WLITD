fa6755728799a6df574556d916fa6945856d1a43
Carson Burnham <gaster.burnham@gmail.com>
2025-02-03 18:52:58 -0600

    attempting to fix eating sound

--- What Lurks In The Dark/Assets/Scripts/Food.cs
+++ What Lurks In The Dark/Assets/Scripts/Food.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public int value;
+    [SerializeField] public AudioSource _as;
+    [SerializeField] public AudioClip _cartoonChompSoundEffect;
     
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("food"))
+        if (other.gameObject.CompareTag("Duncan"))
         {
+            _as.PlayOneShot(_cartoonChompSoundEffect);
             Destroy(gameObject);
             
             
