d87f6eb22fc2a3a1ea5c8f8223cfa2ac505f563c
Carson Burnham <gaster.burnham@gmail.com>
2025-03-17 18:42:56 -0500

    began work on main menu

---
+++ What Lurks In The Dark/Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader 
+{
+public enum Scene 
+{
+    SampleScene,
+}
+
+public static void Load(Scene scene)
+{
+    SceneManager.LoadScene(scene.ToString());
+}
+
+}
